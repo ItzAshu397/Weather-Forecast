83038c31e468ab717cb94b9f29de1f1303057f8f
Ashutosh Swamy <76511506+iamashuu397@users.noreply.github.com>
2023-02-04 12:18:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function showWeather() {
-  let city = document.getElementById("input").value;
-  const url = `https://api.weatherapi.com/v1/current.json?key=c8d2d2c8bb3146d4a35134220222910&q=${encodeURIComponent(
-    city
-  )}&aqi=no`;
+  let location = document.getElementById("location").value;
+  let output = document.getElementById("output");
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      let weather = document.getElementById("weather");
-      weather.innerHTML = `${data.location.name}'s Weather<br><br>Temperature: ${data.current.temp_c} C° or ${data.current.temp_f} F°<br>Wind Speed: ${data.current.wind_kph} kph or ${data.current.wind_mph} mph<br>Wind Degree: ${data.current.wind_degree}°<br>Humidity: ${data.current.humidity}%<br>Pressure: ${data.current.pressure_in} inches<br>Precipitation: ${data.current.precip_in} inches<br><br>${data.location.name}'s Location<br><br>State: ${data.location.region}<br>Country: ${data.location.country}<br>Latitude: ${data.location.lat}<br>Longitude: ${data.location.lon}<br>Date and Time: ${data.location.localtime}`;
-    });
+  if (!location) {
+    output.innerHTML = "Please enter a location";
+  } else {
+    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(
+      location
+    )}/today?unitGroup=metric&include=current&key=2WQD3KTT2UGM668DRP3XW2U77&contentType=json`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then(
+        (data) =>
+          (output.innerHTML = `Location: ${data.resolvedAddress}<br>Latitude: ${data.latitude}<br>Longitude: ${data.longitude}<br>Timezone: ${data.timezone}<br>Date: ${data.days[0].datetime}<br>Temperature: ${data.currentConditions.temp} °C<br>Feels like: ${data.currentConditions.feelslike} °C<br>Humidity: ${data.currentConditions.humidity}%<br>Wind Speed: ${data.currentConditions.windspeed} km/h<br>Wind Direction: ${data.currentConditions.winddir}°<br>Conditions: ${data.currentConditions.conditions}<br>Sunrise Time: ${data.currentConditions.sunrise}<br>Sunset Time: ${data.currentConditions.sunset}`)
+      )
+      .catch((err) => {
+        console.log(err);
+        output.innerHTML = "Data not available for this location";
+      });
+  }
 }
