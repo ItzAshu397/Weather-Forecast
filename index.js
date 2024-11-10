83b28092e172b12b02927a236e0816f9a9d32c28
Ashutosh Swamy <76511506+ashutoshswamy@users.noreply.github.com>
2024-11-11 00:36:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,14 +7,44 @@ function showWeather() {
   } else {
     let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(
       location
-    )}/today?unitGroup=metric&include=current&key=your_api_key&contentType=json`;
+    )}/today?unitGroup=metric&include=current&key=2WQD3KTT2UGM668DRP3XW2U77&contentType=json`;
 
     fetch(url)
-      .then((res) => res.json())
-      .then(
-        (data) =>
-          (output.innerHTML = `Location: ${data.resolvedAddress}<br>Latitude: ${data.latitude}<br>Longitude: ${data.longitude}<br>Timezone: ${data.timezone}<br>Date: ${data.days[0].datetime}<br>Temperature: ${data.currentConditions.temp} °C<br>Feels like: ${data.currentConditions.feelslike} °C<br>Humidity: ${data.currentConditions.humidity}%<br>Wind Speed: ${data.currentConditions.windspeed} km/h<br>Wind Direction: ${data.currentConditions.winddir}°<br>Conditions: ${data.currentConditions.conditions}<br>Sunrise Time: ${data.currentConditions.sunrise}<br>Sunset Time: ${data.currentConditions.sunset}`)
-      )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        output.innerHTML = "";
+
+        document.getElementById(
+          "temperature"
+        ).innerText = `${data.currentConditions.temp} °C`;
+        document.getElementById(
+          "feels-like"
+        ).innerText = `${data.currentConditions.feelslike} °C`;
+        document.getElementById(
+          "humidity"
+        ).innerText = `${data.currentConditions.humidity}%`;
+        document.getElementById(
+          "wind-speed"
+        ).innerText = `${data.currentConditions.windspeed} km/h`;
+        document.getElementById(
+          "wind-direction"
+        ).innerText = `${data.currentConditions.winddir}°`;
+        document.getElementById("condition").innerText =
+          data.currentConditions.conditions;
+        document.getElementById("sunrise").innerText =
+          data.currentConditions.sunrise;
+        document.getElementById("sunset").innerText =
+          data.currentConditions.sunset;
+        document.getElementById("timezone").innerText = data.timezone;
+        document.getElementById("date").innerText = data.days[0].datetime;
+
+        output.innerHTML = `<strong>Location:</strong> ${data.resolvedAddress}`;
+      })
       .catch((err) => {
         console.log(err);
         output.innerHTML = "Data not available for this location";
